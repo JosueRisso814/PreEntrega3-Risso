efffcb9c40b0aebd49b5d6fc22a0179be18ab766
JosueR <jojorissos@gmail.com>
2024-02-03 23:21:44 -0300

    proyecto final

--- index.js
+++ index.js
@@ -1,57 +1,50 @@
-const PRODUCTOS = [
-    {
-        id:1, 
-        nombre:'remera', 
-        precio:5,
-        img: './img/RemeraBlackpink01.webp'
-    },
-    {
-        id:2, 
-        nombre:'buzo', 
-        precio:8,
-        img: './img/buzo.webp'
-    },
-    {
-        id:3, 
-        nombre:'light stick', 
-        precio:12,
-        img: './img/Lightstick.webp'
-    }
-]
+
+//CREACION DE CARDS / LLAMADO DE JSON LOCAL CON FETCH
+
+function cargarProductos (){
+
+fetch('/productos.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(producto => {
+            const div = document.createElement('div');
+            div.classList.add('card');
+            div.innerHTML = `
+            <img class='card-img' src='${producto.img}'>
+            <div>
+                <h3>${producto.nombre}</h3>
+                <p>$${producto.precio.toFixed(2)}</p>
+                <button class='producto-agregar' id='${producto.id}'>AGREGAR</button>
+            </div>
+            `;
+    
+            contenedorProductos.appendChild(div);
+    
+            detectarDarkModeEnLS();
+        });
+
+        actBotonesAgree();
+        SumaTotal()
+    })};
 
 const contenedorProductos = document.querySelector('#contenedor-Productos');
 let botonesAgregar = document.querySelectorAll('.producto-agregar');
 const numeroCarrito = document.querySelector('#cantidadCarrito');
 
+// DETECTAR DARK MODE EN LOCAL STORAGE
 
-//CREACION DE CARDS 
-
-function cargarProductos (){
-
-    PRODUCTOS.forEach(producto => {
-        const div = document.createElement('div');
-        div.classList.add('card');
-        div.innerHTML = `
-        <img class="card-img" src="${producto.img}">
-        <div>
-            <h3>${producto.nombre}</h3>
-            <p>$${producto.precio.toFixed(2)}</p>
-            <button class="producto-agregar" id="${producto.id}">AGREGAR</button>
-        </div>
-        `;
-
-        contenedorProductos.appendChild(div);
-    })
-
-    actBotonesAgree();
-    SumaTotal()
+function detectarDarkModeEnLS (){
+    if (darkMode === 'activado') {
+        activarDarkMode();
+    } else {
+        desactivarDarkMode();
+    }
 }
-
 
 // PARA AGREGAR PRODUCTOS AL CARRITO 1
 
 function actBotonesAgree(){
-    botonesAgregar = document.querySelectorAll(".producto-agregar");
+    botonesAgregar = document.querySelectorAll('.producto-agregar');
 
     botonesAgregar.forEach(boton => {
         boton.addEventListener('click', agregarAlCarrito);
@@ -76,6 +69,21 @@ if (productosEnCarritoLS) {
 
 function agregarAlCarrito(e) {
 
+    Toastify({
+        text: 'Agregado al carrito',
+        duration: 4000,
+        destination: 'https://github.com/apvarun/toastify-js',
+        newWindow: true,
+        close: true,
+        gravity: 'bottom', // `top` or `bottom`
+        position: 'right', // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: 'linear-gradient(to left, rgb(187, 48, 97), #000000)',
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
     const idboton = e.currentTarget.id;
 
     const productoAgregado = PRODUCTOS.find(producto => producto.id == idboton);
@@ -86,7 +94,6 @@ function agregarAlCarrito(e) {
     } else{
         productoAgregado.cantidad = 1;
         carrito.push(productoAgregado);
-        
     };
 
     actualziarNumeroCarrito();
@@ -134,7 +141,7 @@ function VerProductosDelCarrito(){
             contenedorProductosCarrito.appendChild(div);
         });
     } else {
-        console.log("tu carrito esta vacio");
+        console.log('tu carrito esta vacio');
 }
 }
 
@@ -143,45 +150,60 @@ function VerProductosDelCarrito(){
 function SumaTotal() {
     const totalDom = document.querySelector('#total');
     const total = carrito.reduce((acc, producto) => acc+ (producto.precio * producto.cantidad),0);
-    totalDom.innerHTML= `<h3 class='separador'>total: $${total} </h3>`;
+    totalDom.innerHTML= `<h3 class='separador'>TOTAL: $${total} </h3>`;
 };
 
 //PARA BOTON PARA DARK MODE
 
-const botonColorMode = document.querySelector("#colorMode");
+const botonColorMode = document.querySelector('#colorMode');
 
 const body = document.body;
 
-let darkMode = localStorage.getItem("darkMode");
-
+let darkMode = localStorage.getItem('darkMode');
+    
 function activarDarkMode() {
-    body.classList.add("colorModes");
-    botonColorMode.innerText = "CAMBIAR A LIGHT MODE";
-    localStorage.setItem("darkMode", "activado");
+    body.classList.add('colorModes');
+    botonColorMode.innerText = 'CAMBIAR A LIGHT MODE';
+    localStorage.setItem('darkMode', 'activado');
 }
 
 function desactivarDarkMode() {
-    body.classList.remove("colorModes");
-    botonColorMode.innerText = "CAMBIAR A DARK MODE";
-    localStorage.setItem("darkMode", "desactivado");
+    body.classList.remove('colorModes');
+    botonColorMode.innerText ='CAMBIAR A DARK MODE';
+    localStorage.setItem('darkMode', 'desactivado');
 }
 
-if (darkMode === "activado") {
-    activarDarkMode();
-} else {
-    desactivarDarkMode();
-}
 
-botonColorMode.addEventListener("click", () => {
 
-    if (localStorage.getItem("darkMode") === "activado") {
-        desactivarDarkMode();
-    } else {
-        activarDarkMode();
-    }
+botonColorMode.addEventListener('click', () => {
+    localStorage.getItem('darkMode') === 'activado' ? desactivarDarkMode() : activarDarkMode();
 })
 
 cargarProductos();
 
-
 VerProductosDelCarrito();
+
+//MANEJO DE PROMESAS CON FETCH 
+
+const listaDeProductosProximos = document.querySelector('#listaDeProductosProximos');
+
+const ldppID = document.querySelector('#listaDeProductosProximosID')
+
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(data=>{
+                data.forEach(producto => {
+                    const div = document.createElement('div');
+                    div.classList.add('card');
+                    div.innerHTML = `
+            <div>
+                <img class='card-img2' src='${producto.image}'>
+                <div>
+                <p class='parrafoNP'>${producto.title}</p>
+                <p>$${producto.price}</p>
+                </div>
+            </div>
+            `;
+            ldppID.append(div);
+                })
+            });
